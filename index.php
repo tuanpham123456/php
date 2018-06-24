62607fef98d0400081056a121ff81dd63f9adc52
tuanpham123456 <31348703+tuanpham123456@users.noreply.github.com>
2018-06-24 08:19:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,278 @@
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="favicon.ico\2.jpg">
+	<title>Thông tin các loại vắc Xin</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	 crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+	 crossorigin="anonymous">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ"
+	 crossorigin="anonymous"></script>
+	<style>
+		*{
+	padding: 0;
+	margin: 0;
+}
+
+#menu{
+	height: 40px;
+	background-color: #3e9a95;
+}
+
+#menu ul li{
+	line-height: 40px;
+	float: left;
+	padding-left: 110px;
+	list-style: none;
+}
+
+#menu ul li a{
+	text-transform: uppercase;
+	color: #fff;
+	font-weight: bold;
+}
+
+#menu ul li a:hover{
+	text-decoration: none;
+	color: orange;
+	cursor: pointer;
+}
+
+#search{
+	padding-right: 60px;
+	position: relative;
+	z-index: 1;
+}
+
+#search span{
+	position: absolute;
+    top: 7px;
+    left: 10px;
+    z-index: 2;
+}
+
+#search input{
+	padding-left: 30px;
+}
+
+#container{
+	margin: 20px 20px 20px 100px;
+}
+
+h2{
+	text-transform: uppercase;
+	font-weight: bold;
+}
+
+.picture-1{
+	border: 1px solid gray;
+	padding: auto;
+	padding-top: 20px;
+	margin: 10px;
+	text-align: center;
+	width: 22%;
+	float: left;
+	background-color: #fff;
+	position: relative;
+}
+
+.picture-1 img{
+	width: 250px;
+	height: 150px;
+}
+
+.picture-1 p{
+	padding-top: 20px;
+
+}
+
+.picture-1 p:last-child{
+	padding-top: 0px;
+	color: red;
+}
+
+button {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    height: 23px;
+    color: #fff;
+    background-color: #de0303;
+}
+
+.picture-1:hover{
+	background-color: #ecdcdc;
+	cursor: pointer;
+}
+.dropdown .dropbtn {
+    cursor: pointer;
+    font-size: 16px;    
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+}
+
+
+
+	</style>
+		<!-- Latest compiled and minified JavaScript -->
+</head>
+
+<body>
+
+	<div id="menu">
+
+		<ul>
+			<li>
+				<a style="color: royalblue" href="#">CÁC LOẠI VẮC XIN</a>
+
+			</li>
+			<li>
+				<a href="DICHVU.html">CÁC DỊCH VỤ</a>
+			</li>
+			<li>
+				<a href="CAMNANG.html">CẨM NANG TIÊM CHỦNG</a>
+			</li>
+			<li>
+				<a href="BANGGIA.html">BẢNG GIÁ</a>
+			</li>
+			<li>
+				<a href="error.html">ĐẶT LỊCH TIÊM</a>
+			</li>
+			<li>
+				<a href="KHAOSAT.html">KHẢO SÁT DỊCH VỤ</a>
+			</li>
+		</ul>
+	</div>
+
+	<div id="container">
+		<div id="search">
+			<span class="icon">
+				<i class="fa fa-search"></i>
+			</span>
+			<input class="form-control" type="search" id="search" placeholder="Search..." />
+		</div>
+		<div class="form">
+			<h2>Thông tin sản phẩm Vắc Xin</h2>
+			<div class="product">
+
+				<div class=" picture-1">
+
+					<img src="image/INFANRIX-6.jpg">
+					<div class="box-title">
+						<a style="color: red" href="INFANRIX.html">INFANRIX – Vắc xin 6 trong 1 (Phòng 6 bệnh: Ho gà, bạch hầu, uốn ván, bại liệt, viêm gan B và các bệnh do HIB)</a>
+					</div>
+
+
+				</div>
+				<div class=" picture-1">
+					<img src="image/PENTAXIM-5.jpg">
+					<div class="box-title">
+						<a style="color: red" href="PENTAXIM.html	">PENTAXIM – Vắc xin 5 trong 1 (Phòng 5 bệnh: Ho gà, bạch hầu, uốn ván,bại liệt và các bệnh do HIB)</a>
+					</div>
+
+				</div>
+				<div class=" picture-1">
+					<img src="image/VERORAB.jpg">
+					<div class="box-title">
+						<a style="color: red" href="VERORAB.html">VERORAB – Vắc xin phòng dại tinh chế Verorab (Pháp)</a>
+					</div>
+
+					<br>
+
+
+
+				</div>
+				<div class=" picture-1" padding:="" 5px;="" style="padding:  5px;">
+					<img src="image/bcg.jpg">
+					<div class="box-title">
+						<a style="color: red" href="BCG.html">Vacxin BCG đông khô – Vắc xin phòng Lao</a>
+					</div>
+
+
+					<p></p>
+					<BR>
+
+				</div>
+			</div>
+
+			<div class=" picture-1">
+				<img src="image/VARIVAX.jpg">
+				
+				<div class="box-title">
+					<a style="color: red" href="VARIVAX.html">VARIVAX – Vắc xin phòng thủy đậu</a>
+				</div>
+				<p></p>
+				<p></p>
+
+
+			</div>
+			<div class=" picture-1">
+				<img src="image/MMR-II.jpg">
+				<div class="box-title">
+					<a style="color: red" href="MMR2.html">MMR II – Vắc xin phòng 3 bệnh: Sởi, quai bị, rubella</a>
+				</div>
+				
+
+			</div>
+			<div class=" picture-1">
+				<img src="image/Avaxim.jpg">
+				<span style="color: red">AVAXIM – Vắc xin phòng viêm gan A</span>
+				<p></p>
+				<p></p>
+
+			</div>
+			<div class=" picture-1">
+				<img src="image/TWINRIX.jpg">
+				<span style="color: red">TWINRIX – Vắc xin phòng viêm gan A+B</span>
+				<p></p>
+				<p></p>
+
+			</div>
+			<div class="picture-1">
+				<img src="image/PNEUMO-23.jpg">
+				<span style="color:red">PNEUMO 23 – Vắc xin phòng viêm phổi do phế cầu</span>
+				<p></p>
+				<p></p>
+
+			</div>
+			<div class="picture-1">
+				<img src="image/SYNFLORIX.jpg">
+				<span style="color:red">SYNFLORIX – Vắc xin phòng viêm não, viêm phổi, nhiễm khuẩn huyết, viêm tai giữa do H.Influenzae không định tuýp</span>
+				<p></p>
+
+			</div>
+			<div class="picture-1">
+				<img src="image/VA-MENGOC-BC.jpg">
+				<span style="color:red">VA-MENGOC-BC – Vắc xin phòng viêm màng não do não mô cầu B + C</span>
+				<p></p>
+				<p></p>
+
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+	 crossorigin="anonymous"></script>
+</body>
+
+</html>
